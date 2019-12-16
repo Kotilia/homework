d8ebcbf8509460137ee8e21761463f7e527e9490
Kotilia <58958642+Kotilia@users.noreply.github.com>
2019-12-17 00:46:38 +0200

    laba 1

--- first.py
+++ first.py
@@ -1,30 +1,48 @@
-from random import randrange
-import time
-from datetime import datetime
+Kot Ilya Ai-182
 
-#buble
-my_list = [ randrange(0, 15) for i in range(10) ]
+#Сортировка 1
+def BubbleSort(someArray):
+    for i in range(0, len(someArray) - 1):
+        swapCheck = False
+        for j in range(len(someArray) - i - 1):
+            if (someArray[j] > someArray[j + 1]):
+                someArray[j], someArray[j + 1] = someArray[j + 1], someArray[j]
+                SswapCheck = True
+    if (not swapCheck): 
+        swapCheck = False
+    return someArray
 
-max_list = len( my_list )
+myArray = [234, 655, 618, 687, 524, 698, 947, 332, 890, 124, 508, 809, 325, 456, 344]
+print(BubbleSort(myArray))
 
-i = 0
-while i < max_list:
+#Сортировка 2
+def SelectionSort(someArray):
+    for i in range(len(someArray) - 1):
+        minElement = i
+        for j in range(i + 1, len(someArray)):
+            if (someArray[j] < someArray[minElement]):
+                minElement = j
+        if minElement != i:
+            someArray[i], someArray[minElement] = someArray[minElement], someArray[i]
+    return someArray
 
-    j = 0
-    while j < max_list-i-1:
+anotherMyArray = [342, 231, 231, 602, 23, 312, 540, 233, 534, 472, 544, 233, 968, 423, 114]
+taskFromPractice = [50, 80, 19, 86, 35, 7, 60, 48, 51]
+print(SelectionSort(anotherMyArray))
+print(SelectionSort(taskFromPractice))
 
-        if my_list[ j ] > my_list[ j + 1 ]:
+#Сортировка 3
+def InsertionSort(someArray):
+    for i in range(1, len(someArray)):
+        checkingNumber = someArray[i]
+        j = i - 1
+        while j >= 0 and checkingNumber < someArray[j]:
+            someArray[j + 1] = someArray[j]
+            j -= 1
+        someArray[j + 1] = checkingNumber
+    return someArray
 
-            my_list[ j ], my_list[ j + 1] = my_list[ j + 1], my_list[ j ]
-        j+=1
-    i += 1
-
-print( my_list )
-
-
-
-from datetime import datetime
-start_time = datetime.now()
-# do your work here
-end_time = datetime.now()
-print('Duration: {}'.format(end_time - start_time))
+andAnotherAnotherMyArray = [456, 234, 123, 243, 652, 339, 323, 432, 383, 967, 123, 225, 174, 234, 411]
+anotherTaskFromPractice = [77, 89, 74, 68, 70, 49, 5, 62, 51]
+print(InsertionSort(andAnotherAnotherMyArray))
+print(InsertionSort(anotherTaskFromPractice))
